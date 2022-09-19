cfe019d61269bc7b3ff934d5e1a69f706c2416ae
Laskellyb <anikwenzekelly@gmail.com>
2022-09-19 10:26:49 +0100

    The download is now successful because of the html2canvas library I used

---
+++ dummy.js
@@ -0,0 +1,8 @@
+// download card
+const downloadCard = document.querySelector(".download-card");
+downloadCard.addEventListener("click", () => {
+  // const dataURL = cardContainer.toDataURL()
+  // const imgConverted = document.getElementById('imgConverted')
+  // imgConverted.src = dataURL
+  // console.log(dataURL)
+});
